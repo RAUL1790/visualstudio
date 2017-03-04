4937dfdd114011aef908199b671fdc0f60ef38c5
Raul Meneses <amos.1790@gmail.com>
2017-03-04 15:44:56 -0400

    nuevo proyecto

---
+++ pruebagithub/pruebagithub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pruebagithub.Startup))]
+namespace pruebagithub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
